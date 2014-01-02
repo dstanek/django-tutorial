0416b6f9d508a5915c58a85d09efa57f0b474f1b
Mike Pirnat <mpirnat@gmail.com>
2014-01-02 14:23:09 -0500

    Ex 10: Add relations to the Talks model

--- talks/models.py
+++ talks/models.py
@@ -1,4 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class TimeSlot(models.Model):
+
+    slot = models.DateTimeField()
+
+    def __str__(self):
+        return self.slot.strftime("%A, %B %d, %Y @ %I:%M %p")
+
+    # Remove entirely for Python 3
+    def __unicode__(self):
+        try:
+            return unicode(str(self))
+        except NameError:
+            return str(self)
+
+
+class Location(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Remove entirely for Python 3
+    def __unicode__(self):
+        try:
+            return unicode(str(self))
+        except NameError:
+            return str(self)
+
+
+class TalkType(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Remove entirely for Python 3
+    def __unicode__(self):
+        try:
+            return unicode(str(self))
+        except NameError:
+            return str(self)
+
+
+class AudienceSkillLevel(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    # Remove entirely for Python 3
+    def __unicode__(self):
+        try:
+            return unicode(str(self))
+        except NameError:
+            return str(self)
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    # Remove entirely for Python 3
+    def __unicode__(self):
+        try:
+            return unicode(str(self))
+        except NameError:
+            return str(self)
 
 
 class Talk(models.Model):
@@ -7,6 +86,13 @@ class Talk(models.Model):
 
     approved = models.NullBooleanField()
     recording_release = models.BooleanField(default=False)
+
+    talk_type = models.ForeignKey(TalkType)
+    category = models.ForeignKey(Category)
+    audience_skill_level = models.ForeignKey(AudienceSkillLevel)
+    location = models.ForeignKey(Location, blank=True, null=True)
+    time_slot = models.ForeignKey(TimeSlot, blank=True, null=True)
+    proposers = models.ManyToManyField(User)
 
     outline = models.TextField(blank=True)
     abstract = models.TextField(blank=True)
